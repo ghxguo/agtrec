54d3a8ae950a619ba6ecda793bebd307f2e2cb7c
Yi Han <hanyiabc@live.com>
2018-09-23 15:14:33 -0400

    progress, change vector of point to eigen type

--- agbot_deploy-master/src/agbot_nav/src/utilities.cpp
+++ agbot_deploy-master/src/agbot_nav/src/utilities.cpp
@@ -31,33 +31,90 @@ void PPController::initialize(string filename)
     nPts = wpList.size();
     segNormVecList.reserve(nPts);
     tgtHeading.push_back(0);
-        // self.segNormVecList = np.zeros((2,self.nPts))
-
-        // self.tgtHeading.append(0)
-    
     for(size_t i = 0; i < wpList.size(); i++)
     {
-        tgtHeading.push_back()
+        tgtHeading.push_back(0);//Place holder, CHANGE!!
     }
     
 
-        # Loop to compute the target heading values:
-        for idx in range(0, len(self.wpList)-1):
+        // # Loop to compute the target heading values:
+        // for idx in range(0, len(self.wpList)-1):
 
-            self.tgtHeading.append( math.atan2( self.wpList[idx + 1].y - self.wpList[idx].y , self.wpList[idx+1].x - self.wpList[idx].x))
+        //     self.tgtHeading.append( math.atan2( self.wpList[idx + 1].y - self.wpList[idx].y , self.wpList[idx+1].x - self.wpList[idx].x))
 
-            normX = self.wpList[idx].y - self.wpList[idx + 1].y
-            normY = self.wpList[idx + 1].x - self.wpList[idx].x     #bug might live here
+        //     normX = self.wpList[idx].y - self.wpList[idx + 1].y
+        //     normY = self.wpList[idx + 1].x - self.wpList[idx].x     #bug might live here
 
-            # Calculate the norm:
-            nVecMag = np.sqrt( normX**2 + normY**2)
+        //     # Calculate the norm:
+        //     nVecMag = np.sqrt( normX**2 + normY**2)
 
-            self.segNormVecList[0,idx+1] = normX/nVecMag
-            self.segNormVecList[1,idx+1] = normY/nVecMag
+        //     self.segNormVecList[0,idx+1] = normX/nVecMag
+        //     self.segNormVecList[1,idx+1] = normY/nVecMag
 
-        self.tgtHeading[0] = self.tgtHeading[1]
-        self.segNormVecList[:,0] = self.segNormVecList[:,1]
+        // self.tgtHeading[0] = self.tgtHeading[1]
+        // self.segNormVecList[:,0] = self.segNormVecList[:,1]
 }
+
+void PPController::compute_steering_vel_cmds(Point current, double &vel, double &delta, double &distance2Goal)
+{
+    // Compute vector from current position to current waypoint:
+    VectorXd vecRobot2WP = VectorXd::Zero(2,1);
+    vecRobot2WP(0,0) = this->wpList[this->currWpIdx].x-current.x;
+    vecRobot2WP(1,0) = this->wpList[this->currWpIdx].y - current.y;
+    VectorXd vecCurHeading = VectorXd::Zero(2,1);
+    vecRobot2WP(0,0) = this->wpList[this->currWpIdx].x-current.x;
+    vecRobot2WP(1,0) = this->wpList[this->currWpIdx].y - current.y;
+    
+    vecCurHeading(0,0) = cos(this->tgtHeading[this->currWpIdx]);
+    vecCurHeading(1,0) = sin(this->tgtHeading[this->currWpIdx]);
+    distance2aGoal = vecRobot2WP.dot(vecCurHeading);
+    cout<<"distance2Goal" << distance2aGoal;
+
+    //Compute the minimum distance from the current segment:
+    //change this
+    double minDist = vecRobot2WP.dot(segNormVecList[:,self.currWpIdx]);
+    double theta_gain = this->k_theta * minDist;
+        if (theta_gain > M_PI/2)
+        {
+            theta_gain = M_PI/2;
+        }
+        if(theta_gain < -M_PI/2)
+        {
+            theta_gain = -M_PI/2;    
+        }
+        cout << "minDist = " << minDist;
+        cout << "theta_gain =" << theta_gain;
+        //Compute the desired heading angle based of target heading and the min dist:
+        auto theta_des = this->tgtHeading[this->currWpIdx] + theta_gain;
+
+        cout << "Theta des = ",theta_des);
+        //Compute the steering agle command:
+
+        //change this
+        auto heading_err = theta_des - current.heading;
+
+        if(heading_err > M_PI)
+        {
+            heading_err = heading_err - 2 * M_PI;
+        }
+        else if(heading_err < -M_PI)
+        heading_err = heading_err + 2 * M_PI;
+        delta = this->k_delta*(heading_err);
+
+        cout << "Target heading = " << this->tgtHeading[this->currWpIdx];
+
+        //Compute forward velocity:
+        vel = this->maximumVelocity - abs(this->k_vel * delta);
+
+        if (vel < this->minVelocity)
+            vel = this->minVelocity;
+        if (delta > 1)
+            delta = 1;
+        if (delta < -1)
+            delta = -1;
+
+}
+
 
 PPController::~PPController()
 {
--- agbot_deploy-master/src/agbot_nav/src/utilities.h
+++ agbot_deploy-master/src/agbot_nav/src/utilities.h
@@ -4,15 +4,18 @@
 #include <cmath>
 #include <vector>
 #include <fstream>
+#include <iostream>
 #include <string>
 #include <Eigen/Dense>
+#define _USE_MATH_DEFINES
 
+using std::cout;
 using std::vector;
 using std::ifstream;
 using std::string;
 using std::stod;
 using Eigen::MatrixXd;
-
+using Eigen::VectorXd;
 
 
 //struct to define vehicle position on a coordinate system at a certain heading
@@ -21,6 +24,7 @@ struct Point
     int x;
     int y;
     int inputHeading;
+    Point() : x(0), y(0), inputHeading(0) {}
     Point(int xIn, int yIn, int headingIn): x(xIn), y(yIn), inputHeading(headingIn) { }
     Point(int xIn, int yIn): x(xIn), y(yIn) { }
 
@@ -39,13 +43,14 @@ struct AckermannVehicle
 class PPController
 {
 private:
-    double leadDistance, length, turningRadius, maximumVelocity;
+    double leadDistance, length, turningRadius, maximumVelocity;//set the length for ppcontroller as the length of maximumSteeringAngle
+
     //List of waypoints: From start to end:
     vector<Point> wpList;
     //Current target waypoint index:
     int currWpIdx;
-    //List of normal vectors to segments joining the waypoints:
-    vector<int> tgtHeading;
+    //List of desired heading values:
+    vector<double> tgtHeading;
     //List of normal vectors to segments joining the waypoints:
     vector<Point> segNormVecList;
     //Number of waypoints:
@@ -61,6 +66,23 @@ private:
 public:
     PPController(double inputLeadDistance, double inputLength = 2.065, double inputMinTurningRadius = 4.6, double inputMaximumVelocity = 0.5);
     void initialize(string filename);
+    
+    //Function to compute steering angle and forward velocity commands:
+    //References are all return values  
+    void compute_steering_vel_cmds(Point current, double &vel, double &delta, double &distance2Goal);
+
+    //compute the steering radius of ackerman vehicle of given parameters
+    void compute_turning_radius(Point current = Point(0,0,0) , Point goal = Point(0,0,0));
+
+    //compute the steering angle of ackermann vehicle of given paramters
+    double compute_steering_angle();
+    
+    //compute forward velocity relative to steering angle
+    double compute_forward_velocity(); //added a variable velocity based on Bijo's suggestion
+
+    //return the unit vector of the vector
+    VectorXd unit_vector(VectorXd vector);
+
     ~PPController();
 };
 
